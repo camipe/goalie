43d3fe96a99ba0592def44bfe631fb0ad5d0edbb
Micael Persson <micael.c.persson@gmail.com>
2018-06-01 14:47:47 +0200

    created struct for goal.

--- contracts/GoalFactory.sol
+++ contracts/GoalFactory.sol
@@ -3,9 +3,18 @@ import "./Ownable.sol";
 
 
 contract GoalFactory is Ownable {
+  
   event NewGoal();
 
   // goal struct
+  struct Goal {
+    string title;
+    string description;
+    address owner;
+    address[] friends;
+    uint amount;
+    uint deadline;
+  }
 
   // goal array
 
@@ -15,3 +24,13 @@ contract GoalFactory is Ownable {
 
   // function newGoal()
 }
+
+    address parent;
+    address public user;
+    address public trustee;
+    address public beneficiary;
+    string public title;
+    string public description;
+    uint public amount;
+    uint public deadline;
+Status public status;
