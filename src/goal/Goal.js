c747937342d12c467490084f44da18b2e2533e61
Micael Persson <micael.c.persson@gmail.com>
2018-06-15 14:56:40 +0200

    Improved goal component.

--- src/goal/Goal.js
+++ src/goal/Goal.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Goal = (props) => {
-    return (
-      <p>This is a goal</p>
-    )
+  return (
+    <div>
+      <p>
+        Title: {props.goal.title}<br/>
+        Description: {props.goal.description}<br/>
+        Beneficiary: {props.goal.beneficiary}<br/>
+        Amount: {props.goal.amount}<br/>
+        Deadline:{props.goal.deadline}<br/>
+        {props.goal.approvals.length} of {props.goal.nrOfFriends} approvals.
+      </p>
+      <button>Complete</button>
+      <button>Approve</button>
+      <button>Pay out</button>
+    </div>
+  )
 }
 
-export default Goal
+Goal.propTypes = {
+  goal: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    owner: PropTypes.string,
+    beneficiary: PropTypes.string,
+    amount: PropTypes.number,
+    deadline: PropTypes.number,
+    nrOfFriends: PropTypes.number,
+    approvals: PropTypes.array,
+  }),
+  isOwner: PropTypes.bool,
+  isBeneficiary: PropTypes.bool,
+  isFriend: PropTypes.bool
+}
+
+export default Goal;
--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
-import { AccountData, ContractData, ContractForm } from 'drizzle-react-components'
 import logo from '../../logo.png'
 
 import Goal from "../../goal/Goal";
 
 class Home extends Component {
+  constructor( props ) {
+    super( props )
+
+    // Apply background filters
+    // document.getElementById( 'backgroundImage' ).setAttribute( 'style', '-webkit-filter: ' + C.BG_FILTER )
+
+    this.state = {
+      testGoal: {
+        title: "Testing",
+        description: "Just testing component",
+        owner: "0xc8DaE4BEc5EAe38C25991C0f7eEb0f683eA4FF85",
+        beneficiary: "0x247080353466978b5a3B27F8B74898a07607ddEc",
+        amount: 123241231,
+        deadline: 1529066379,
+        nrOfFriends: 2,
+        approvals: ["0x9d30613A0d005691237dccF99115c5b5c561E434"],
+      }
+    }
+  }
   render() {
     return (
       <main className="container">
         <div className="pure-g">
           <div className="pure-u-1-1 header">
             <img src={logo} alt="drizzle-logo" />
-            <Goal></Goal>
+            <Goal goal={this.state.testGoal}></Goal>
           </div>
 
           {/* <div className="pure-u-1-1">
