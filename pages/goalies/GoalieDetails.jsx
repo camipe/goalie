c5013919d1ebf0ed106cd4a2ececd4a554e15a43
Micael Persson <micael.c.persson@gmail.com>
2018-10-09 20:43:45 +0200

    basic styling for goalie details.

--- pages/goalies/GoalieDetails.jsx
+++ pages/goalies/GoalieDetails.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'next/router';
+import {
+  Grid,
+  Statistic,
+  Card,
+  Header,
+  Button,
+} from 'semantic-ui-react';
 import web3 from '../../ethereum/web3';
 import Goalie from '../../ethereum/goalie';
 import Layout from '../../components/Layout';
@@ -13,34 +20,70 @@ class GoalieDetails extends Component {
 
     const goalie = Goalie(address);
     const details = await goalie.methods.details().call();
+    const value = '1';
 
-    return { details };
+    return { details, value };
   }
 
-  propTypes = {
+  static propTypes = {
     details: PropTypes.objectOf(PropTypes.node).isRequired,
+    value: PropTypes.number.isRequired,
   }
 
   render() {
-    const { details } = this.props;
-    console.log(details);
+    const { details, value } = this.props;
     return (
       <Layout>
-        <p>
-          Goalie details:
-          {details.title}
-          <br />
-          {details.description}
-          <br />
-          {details.deadline}
-          <br />
-          {details.owner}
-          <br />
-          {details.beneficiary}
-          <br />
-          {details.friend}
-          <br />
-        </p>
+        <Grid>
+          <Grid.Row columns="2">
+            <Grid.Column width="12">
+              <Card fluid>
+                <Card.Content>
+                  <Card.Header>{details.title}</Card.Header>
+                  <Card.Meta>{`Expires at ${details.deadline}`}</Card.Meta>
+                </Card.Content>
+                <Card.Content>
+                  <Grid>
+                    <Grid.Row columns="2">
+                      <Grid.Column width="12">
+                        <Header sub>Description</Header>
+                        {details.description}
+                      </Grid.Column>
+                      <Grid.Column width="4" textAlign="center">
+                        <Statistic horizontal label="Ether" value={value} />
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                </Card.Content>
+                <Card.Content>
+                  <Header sub>Addresses</Header>
+                  {`Owner: ${details.owner}`}
+                  <br />
+                  {`Beneficiary: ${details.beneficiary}`}
+                  <br />
+                  {`Friend:${details.friend}`}
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+            <Grid.Column width="4">
+              <Card fluid>
+                <Card.Content extra>
+                  <Card.Header>Status</Card.Header>
+                  <p>Approval: True</p>
+                  <p>Completed: False</p>
+                  <div className="ui two buttons">
+                    <Button basic color="green">
+                      Approve
+                    </Button>
+                    <Button basic color="red">
+                      Decline
+                    </Button>
+                  </div>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Layout>
     );
   }
