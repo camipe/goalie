20d682989c28f5c7b4cc9dcf87d407aa5ba3b98e
Micael Persson <micael.c.persson@gmail.com>
2018-10-11 17:44:50 +0200

    improved the new goalie form.

--- pages/goalies/GoalieForm.jsx
+++ pages/goalies/GoalieForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Button, Form, Message } from 'semantic-ui-react';
+import { DateInput } from 'semantic-ui-calendar-react';
 import { withRouter } from 'next/router';
+import moment from 'moment';
 import GoalieFactory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 import Layout from '../../components/Layout';
@@ -25,10 +27,13 @@ class GoalieForm extends Component {
 
     this.setState({ loading: true, errorMessage: '' });
 
+    // deadline needs to be converted to seconds since unix epoch because that is used in solidity
+    const formattedDeadline = moment(deadline).unix();
+
     try {
       const accounts = await web3.eth.getAccounts();
       await GoalieFactory.methods
-        .createGoalie(title, description, deadline, beneficiary, friend)
+        .createGoalie(title, description, formattedDeadline, beneficiary, friend)
         .send({
           from: accounts[0],
           value: web3.utils.toWei(amount, 'ether'),
@@ -57,36 +62,46 @@ class GoalieForm extends Component {
             name="title"
             onChange={this.handleInputChange}
             value={title}
+            placeholder="Title"
           />
-          <Form.Input
+          <Form.TextArea
             label="Description"
             name="description"
             onChange={this.handleInputChange}
             value={description}
+            placeholder="Description"
           />
           <Form.Input
             label="Beneficiary"
             name="beneficiary"
             onChange={this.handleInputChange}
             value={beneficiary}
+            placeholder="Beneciary address"
           />
           <Form.Input
             label="Friend"
             name="friend"
             onChange={this.handleInputChange}
             value={friend}
+            placeholder="Friend address"
           />
-          <Form.Input
-            label="Deadline"
-            name="deadline"
-            onChange={this.handleInputChange}
-            value={deadline}
-          />
+          <Form.Field>
+            <label htmlFor="deadline">Deadline</label>
+            <DateInput
+              name="deadline"
+              placeholder="Deadline"
+              value={deadline}
+              iconPosition="left"
+              dateFormat="YYYY-MM-DD"
+              onChange={this.handleInputChange}
+            />
+          </Form.Field>
           <Form.Input
             label="Amount"
             name="amount"
             onChange={this.handleInputChange}
             value={amount}
+            placeholder="Amount of ether"
           />
           <Message error header="Oops!" content={errorMessage} />
           <Button primary loading={loading}>
