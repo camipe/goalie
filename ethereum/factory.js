013277592fc55535390b4ea9e213e47912e01f54
Micael Persson <micael.c.persson@gmail.com>
2018-11-03 15:32:53 +0100

    redeployed factory.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import goalieFactory from './build/GoalieFactory.json';
 
 const GoalieFactory = new web3.eth.Contract(
   JSON.parse(goalieFactory.interface),
-  '0x69F179E17319529AD5b8832d8dBcB9BB179f39D2',
+  '0xC3911C7177e59F60159CCEfd380aB902C8eA9d84',
 );
 
 export default GoalieFactory;
